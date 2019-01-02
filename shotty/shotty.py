21fc87fab0582e5ded0430cbd103d1c7c6386e80
Schiestel <adam.schiestel@vistraenergy.com>
2019-01-02 07:18:29 -0600

    Added reboot command and --force option

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -20,9 +20,15 @@ def has_pending_snapshot(volume):
 	snapshots = list(volume.snapshots)
 	return snapshots and snapshots[0].state == 'pending'
 
+
 @click.group()
-def cli():
-	"""Shotty manages snapshots"""
+@click.option('--profile', default='shotty',
+			  help="Specify a profile other than 'shotty'")
+def cli(profile):
+	"""establish session"""
+	session = boto3.Session(profile_name=profile)
+	ec2 = session.resource('ec2')
+
 
 @cli.group('snapshots')
 def snapshots():
@@ -81,26 +87,37 @@ def instances():
 	help="Create snapshots of all volumes")
 @click.option('--project', default=None,
 	help="Only instances for project (tag Project:<name>)")
-def create_snapshots(project):
+@click.option('--force', is_flag=True,
+			  help="All instances")
+def create_snapshots(project, force):
 	"Create snapshots for EC2 instances"
 	instances = filter_instances(project)
+
+	if project or force:
+		for i in instances:
+			print("Stopping {0}...".format(i.id))
+			i.stop()
+			i.wait_until_stopped()
+			for v in i.volumes.all():
+				if has_pending_snapshot(v):
+					print(" Skipping {0}, snapshot already in progress".format(v.id))
+					continue
+				print("Creating snapshot of {0}...".format(v.id))
+				try:
+					v.create_snapshot(Description="Created by Snapshotalyzer")
+				except botocore.exceptions.ClientError as e:
+					print(" Could not create snapshot {0}. ".format(v.id) + str(e))
+					continue
+
+			print("Starting {0}...".format(i.id))
+			i.start()
+			i.wait_until_running()
 	
-	for i in instances:
-		print("Stopping {0}...".format(i.id))
-		i.stop()
-		i.wait_until_stopped()
-		for v in i.volumes.all():
-			if has_pending_snapshot(v):
-				print(" Skipping {0}, snapshot already in progress".format(v.id))
-				continue
-			print("Creating snapshot of {0}...".format(v.id))
-			v.create_snapshot(Description="Created by Snapshotalyzer")
-		print("Starting {0}...".format(i.id))
-		i.start()
-		i.wait_until_running()
-	
-	print("Complete!")
-	
+		print("Complete!")
+
+	if project is None and force is False:
+		print("This command requires and option. Please use --help for more information")
+
 	return
 
 	
@@ -127,36 +144,88 @@ def list_instances(project):
 @instances.command('stop')
 @click.option('--project', default=None,
 	help="Only instances for project")
-def stop_instaces(project):
+@click.option('--force', is_flag=True,
+			  help="All instances")
+def stop_instaces(project, force):
 	"Stop EC2 instances"
 	instances = filter_instances(project)
-			
-	for i in instances:
-		print("Stopping {0}...".format(i.id))
-		try:
-			i.stop()
-		except botocore.exceptions.ClientError as e:
-			print(" Could not stop {0}. ".format(i.id) + str(e))
-			continue
+
+	if project or force:
+		for i in instances:
+			print("Stopping {0}...".format(i.id))
+			try:
+				i.stop()
+			except botocore.exceptions.ClientError as e:
+				print(" Could not stop {0}. ".format(i.id) + str(e))
+				continue
+
+	if project is None and force is False:
+		print("This command requires and option. Please use --help for more information")
 
 	return
+
 
 @instances.command('start')
 @click.option('--project', default=None,
 	help="Only instances for project")
-def start_instaces(project):
+@click.option('--force', is_flag=True,
+			  help="All instances")
+def start_instaces(project, force):
 	"Start EC2 instances"
 	instances = filter_instances(project)
-			
-	for i in instances:
-		print("Starting {0}...".format(i.id))
-		try:
-			i.start()
-		except botocore.exceptions.ClientError as e:
-			print(" Could not start {0}. ".format(i.id) + str(e))
-			continue
+
+	if project or force:
+		for i in instances:
+			print("Starting {0}...".format(i.id))
+			try:
+				i.start()
+			except botocore.exceptions.ClientError as e:
+				print(" Could not start {0}. ".format(i.id) + str(e))
+				continue
+
+	if project is None and force is False:
+		print("This command requires and option. Please use --help for more information")
 
 	return
+
+
+@instances.command('reboot')
+@click.option('--id', 'server_id', default=None,
+			  help="The instance ID")
+@click.option('--project', default=None,
+			  help="All instances for project")
+@click.option('--force', is_flag=True,
+			  help="All instances")
+def reboot_instaces(project, server_id, force):
+	"reboot EC2 instance"
+	instances = filter_instances(project)
+
+	if server_id:
+		for i in instances:
+			if i.id == server_id:
+				print("Rebooting {0}...".format(i.id))
+				try:
+					i.reboot(DryRun=False)
+					return
+				except botocore.exceptions.ClientError as e:
+					print(" Could not reboot {0} ".format(i.id) + str(e))
+					break
+
+	if project or force:
+		for i in instances:
+			print("Rebooting {0}...".format(i.id))
+			try:
+				i.reboot()
+			except botocore.exceptions.ClientError as e:
+				print(" Could not start {0}. ".format(i.id) + str(e))
+				continue
+
+	if server_id is None and project is None and force is False:
+		print("This command requires and option. Please use --help for more information")
+
+	return
+
+
 
 if __name__ == '__main__':
 	cli()
